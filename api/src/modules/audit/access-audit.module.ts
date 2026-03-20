b257c903d14b0769b4a134e61570d5201f959b78
khanhsvan <118794637+khanhsvan@users.noreply.github.com>
2026-03-21 01:15:16 +0700

    Delete api directory

--- api/src/modules/audit/access-audit.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AccessAuditService } from './access-audit.service';
-
-@Module({
-  providers: [AccessAuditService],
-  exports: [AccessAuditService]
-})
-export class AccessAuditModule {}
-
--- api/src/modules/videos/dto/list-videos.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEnum, IsOptional } from 'class-validator';
-import { VideoType } from '@netflix-mini/types';
-
-export class ListVideosDto {
-  @IsOptional()
-  @IsEnum(VideoType)
-  type?: VideoType;
-}
-
